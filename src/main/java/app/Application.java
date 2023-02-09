88232de662577d7d674f5866b670dacd1e76607b
zumag <artem2522462@mail.ru>
2023-02-09 16:22:25 +0900

    Commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,6 +4,7 @@ import app.config.AppConfig;
 import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
@@ -16,5 +17,4 @@ public class Application {
             bean.whatAnimalSay();
         }
     }
-
 }
